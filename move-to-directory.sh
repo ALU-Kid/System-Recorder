d84c7d8e95d248b6d84982b354df04a060603a9d
ALU-Kid <156905882+ALU-Kid@users.noreply.github.com>
2024-02-21 20:24:04 +0200

    dadaddadsfs

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mv main.sh negpod_id-q1/
-mv Students-list_1023.txt negpod_id-q1/
-mv Select-emails.sh negpod_id-q1/
+mv students-list_1023.txt negpod_id-q1/
+mv select-emails.sh negpod_id-q1/
 mv student-emails.txt negpod_id-q1/

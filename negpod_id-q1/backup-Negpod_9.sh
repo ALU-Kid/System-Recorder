a4d4463901666b755f9b1f586fdc9ea8da901989
ALU-Kid <156905882+ALU-Kid@users.noreply.github.com>
2024-02-21 19:55:07 +0200

    here we go

---
+++ negpod_id-q1/backup-Negpod_9.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Host: 64293e56bc62.3a2627c1.alu-cod.online
+Username:  64293e56bc62
+Password: 328d3b338a4ced526c9a
+Location for backing up of your directory: /summative/1023-2024j
+sshpass -p $Password scp -r $Username@$Host:$Location for backing up of your directory
+

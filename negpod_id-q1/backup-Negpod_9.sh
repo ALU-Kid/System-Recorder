f40d1230018eb9492f3c65d847c516616453a400
ALU-Kid <156905882+ALU-Kid@users.noreply.github.com>
2024-02-23 15:15:56 +0200

    dafgs

--- negpod_id-q1/backup-Negpod_9.sh
+++ negpod_id-q1/backup-Negpod_9.sh
@@ -3,24 +3,10 @@
 # Define source directory
 source_dir="negpod_id-q1"
 
-# Remote server details
-host="64293e56bc62.3a2627c1.alu-cod.online"
-username="64293e56bc62"
-password="328d3b338a4ced526c9a"
-destination="/summative/1023-2024j"
+# Define remote server details
+remote_user="64293e56bc62"
+remote_host="64293e56bc62.3a2627c1.alu-cod.online"
+remote_dir="/summative/1023-2024j"
 
-# Create a temporary file to store the output of the backup test
-output_file=$(mktemp)
-
-# Use ssh to run the backup test on the remote server
-ssh -o "PasswordAuthentication=no" -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=no" -i ~/.ssh/id_rsa "$username@$host" "if [ -d \"$destination/$source_dir\" ]; then echo 'Backup successful'; else echo 'Backup failed'; fi" > "$output_file"
-
-# Check the output of the backup test
-if grep -q 'Backup successful' "$output_file"; then
-    echo 'Backup successful'
-else
-    echo 'Backup failed'
-fi
-
-# Remove the temporary output file
-rm "$output_file"
+# Perform the backup using rsync
+rsync -avz --delete -e "sshpass -p '328d3b338a4ced526c9a' ssh -o StrictHostKeyChecking=no" $source_dir $remote_user@$remote_host:$remote_dir

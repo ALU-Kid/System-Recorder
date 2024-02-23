fc2fff3c197cf7c95522e0980e6c43e966763916
MizeroR <r.mizero@alustudent.com>
2024-02-23 04:35:59 -0800

    5

--- negpod_id-q1/backup-Negpod_9.sh
+++ negpod_id-q1/backup-Negpod_9.sh
@@ -4,9 +4,9 @@
 source_dir="negpod_id-q1"
 
 # Remote server details
-host= 64293e56bc62.3a2627c1.alu-cod.online
-username= 64293e56bc62
-password= 328d3b338a4ced526c9a
+host=64293e56bc62.3a2627c1.alu-cod.online
+username=64293e56bc62
+password=328d3b338a4ced526c9a
 destination=/summative/1023-2024j
 
 # Use scp to copy the source directory to the remote location

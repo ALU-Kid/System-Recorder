62d2322369579835e2f81f994f2f121e76a9904c
ALU-Kid <156905882+ALU-Kid@users.noreply.github.com>
2024-02-23 15:18:22 +0200

    dafsf

--- negpod_id-q1/backup-Negpod_9.sh
+++ negpod_id-q1/backup-Negpod_9.sh
@@ -8,5 +8,5 @@ remote_user="64293e56bc62"
 remote_host="64293e56bc62.3a2627c1.alu-cod.online"
 remote_dir="/summative/1023-2024j"
 
-# Perform the backup using rsync
-rsync -avz --delete -e "sshpass -p '328d3b338a4ced526c9a' ssh -o StrictHostKeyChecking=no" $source_dir $remote_user@$remote_host:$remote_dir
+# Perform the backup using scp
+scp -r $source_dir $remote_user@$remote_host:$remote_dir

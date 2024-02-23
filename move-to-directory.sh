368274a75dc9898de583cb06d268bf797258b3e5
ALU-Kid <156905882+ALU-Kid@users.noreply.github.com>
2024-02-23 13:28:50 +0200

    moving stuff

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+#move file into folder
 mv main.sh negpod_id-q1/
+
+#move file into folder
 mv students-list_1023.txt negpod_id-q1/
+
+#move file into folder
 mv select-emails.sh negpod_id-q1/
+
+#move file into folder
 mv student-emails.txt negpod_id-q1/

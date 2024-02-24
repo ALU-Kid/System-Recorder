0736049d3c60b6229fd3df4362848c29bc67bf90
ALU-Kid <156905882+ALU-Kid@users.noreply.github.com>
2024-02-24 13:14:21 +0200

    awawa

--- negpod_id-q1/backup-Negpod_9.sh
+++ negpod_id-q1/backup-Negpod_9.sh
@@ -8,5 +8,8 @@ remote_user="64293e56bc62"
 remote_host="64293e56bc62.3a2627c1.alu-cod.online"
 remote_dir="/summative/1023-2024j"
 
-# Perform the backup using rsync
-rsync -avz --delete -e "sshpass -p '328d3b338a4ced526c9a' ssh -o StrictHostKeyChecking=no" $source_dir $remote_user@$remote_host:$remote_dir
+# Perform the backup using scp
+scp -r $source_dir $remote_user@$remote_host:$remote_dir
+
+# Test if backup was successful
+ssh $remote_user@$remote_host "if [ -d \"$remote_dir/$source_dir\" ]; then echo 'Backup successful'; else echo 'Backup failed'; fi"

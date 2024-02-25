9b02489659a4732c798b5274b20a5cfa5278811d
TKelia <155954533+TKelia@users.noreply.github.com>
2024-02-25 22:16:56 +0200

    Update backup-Negpod_9.sh

--- backup-Negpod_9.sh
+++ backup-Negpod_9.sh
@@ -8,7 +8,7 @@ remote_user="64293e56bc62"
 remote_host="64293e56bc62.3a2627c1.alu-cod.online"
 remote_dir="/summative/1023-2024j"
 
-# Perform the backup using scp
+# Use scp to complete the backup
 scp -r $source_dir $remote_user@$remote_host:$remote_dir
 
 # Test if backup was successful
